18b643ef260f7be1ca2bd7f973245a7d1a3f78ab
luizcmenezes <lcmscontas@gmail.com>
2020-11-04 20:53:47 -0300

    Implementando login

--- gerenciador/WebContent/WEB-INF/view/listaEmpresas.jsp
+++ gerenciador/WebContent/WEB-INF/view/listaEmpresas.jsp
@@ -12,11 +12,10 @@
 </head>
 <body>
 
+	Lista de empresas: <br />
 	<c:if test="${not empty empresa}">
 		Empresa ${ empresa } cadastrada com sucesso!
 	</c:if>
-	
-	Lista de empresas: <br />
 	
 	<ul>
 		<c:forEach items="${empresas}" var="empresa">
--- gerenciador/src/br/com/alura/gerenciador/modelo/Banco.java
+++ gerenciador/src/br/com/alura/gerenciador/modelo/Banco.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
 	
 	static {
@@ -18,6 +19,14 @@ public class Banco {
 		empresa2.setNome("Caelum");
 		lista.add(empresa);
 		lista.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("nico");
+		u1.setSenha("12345");
+		
+		Usuario u2 = new Usuario();
+        u2.setLogin("ana");
+        u2.setSenha("12345");
 	}
 
 	public void adiciona(Empresa empresa) {
@@ -50,5 +59,14 @@ public class Banco {
 		}
 		return null;
 	}
+
+    public Usuario existeUsuario(String login, String senha) {
+        for (Usuario usuario : listaUsuarios) {
+            if (usuario.ehIquals(login, senha)) {
+                return usuario;
+            }
+        }
+        return null;
+    }
 
 }
