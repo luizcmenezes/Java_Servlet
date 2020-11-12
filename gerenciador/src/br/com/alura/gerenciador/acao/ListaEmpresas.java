f1b376796965bc56e0ff90330ed97a13d648d925
luizcmenezes <lcmscontas@gmail.com>
2020-11-12 20:37:30 -0300

    introdução web service

--- gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
